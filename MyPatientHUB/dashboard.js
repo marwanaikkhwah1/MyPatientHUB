cfc9c91e24644e34140d1b40e9143a84bed6d88f
marwa nadim <marwanaikkhwah@gmail.com>
2026-09-18 22:43:01 +0430

    Add MyPatientHUB dashboard

---
+++ MyPatientHUB/dashboard.js
@@ -0,0 +1,18 @@
+const menuBtn = document.getElementById("menubtn");
+const sidebar = document.querySelector(".sidebar");
+
+menuBtn.addEventListener("click", function () {
+
+    if (window.innerWidth <= 768) {
+        sidebar.classList.toggle("mobile-open");
+    } else {
+        sidebar.classList.toggle("collapsed");
+        document.body.classList.toggle("sidebar-collapsed");
+    }
+
+});
+
+
+logoutBtn.addEventListener("click", function () {
+    window.location.href = "../Medical Clinic/clinic.html";
+});
